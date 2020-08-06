f91204fd477378d6a12a4b4072470143f5d2462e
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2020-08-06 21:33:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+
+
+function App() {
+  const[data, setData] = useState([
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ]);
+
+  return(
+    <div>
+      {data.map((row, oneIndex) => {
+    return (
+      <div style={{ display: "flex" }} key={oneIndex}>
+        {row.map((digit, index) => (
+          <Block num={digit} key={index} />
+        ))}
+      </div>
+      );
+    })}
+    </div>
+  );
+}
+
+const Block= ({num}) => {
+    return <div> (num)</div>
+}
+export default App;
