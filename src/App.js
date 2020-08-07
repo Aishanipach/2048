dd251983307627f12041efacac2b3673fd50f5c6
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2020-08-07 18:02:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,16 @@ function App() {
   ]);
 
   return(
-    <div>
+    <div
+      style={{
+        background: "#AD9D8F",
+        width: "max-content",
+        margin: "auto",
+        padding: 5,
+        borderRadius: 5,
+        marginTop: 10,
+        }}
+      >
       {data.map((row, oneIndex) => {
     return (
       <div style={{ display: "flex" }} key={oneIndex}>
@@ -25,7 +34,36 @@ function App() {
   );
 }
 
-const Block= ({num}) => {
-    return <div> (num)</div>
-}
+const Block = ({ num }) => {
+  const { blockStyle } = style;
+
+  return (
+    <div
+      style={{
+        ...blockStyle,
+        //background: getColors(num),
+        color: num === 2 || num === 4 ? "#645B52" : "#F7F4EF",
+      }}
+    >
+      {num}
+      {/*num !== 0 ? num : ""*/}
+    </div>
+  );
+};
+
+const style = {
+  blockStyle: {
+    height: 80,
+    width: 80,
+    background: "lightgray",
+    margin: 3,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: 45,
+    fontWeight: "800",
+    color: "white",
+  },    
+} 
 export default App;
+
